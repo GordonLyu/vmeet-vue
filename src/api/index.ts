cbb159cba4c89066f533f5b9560f342600b7f8bf
LvXinming <1533159440@qq.com>
2023-06-02 10:54:03 +0800

    添加部分封装的api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import * as mockTest from './mock/test'
 import * as mockUser from './mock/user'
+import * as mockMessage from './mock/message'
+import * as mockContact from './mock/contact'
 import * as socket from './socket'
 import * as user from './request/user'
 
 export default {
-    mockTest, mockUser, socket, user
+    mockTest, mockUser, mockMessage, mockContact, socket, user
 }
--- src/api/request/user.ts
+++ src/api/request/user.ts
@@ -1,21 +1,43 @@
 import type { Login, Register } from "@/types/User"
 import request from "@/utils/request"
 
+/**
+ * 登录
+ * @param userLogin 用户登录数据
+ */
 export const login = (userLogin: Login) => {
-    return request.post('/login', JSON.stringify(userLogin), {
-        headers: {
-            "Content-Type": 'application/json'
-        }
-    })
+    return request.post('/user/login', JSON.stringify(userLogin))
 }
 
+/**
+ * 注册
+ * @param userRegister 注册数据
+ */
 export const register = (userRegister: Register) => {
-    console.log(userRegister);
-    return request.post('/register', JSON.stringify(userRegister), {
-        headers: {
-            "Content-Type": 'application/json'
-        }
-    })
+    return request.post('/user/register', JSON.stringify(userRegister))
+}
+
+/**
+ * 修改密码
+ * @param PasswordData 新密码数据
+ */
+export const changePassword = (PasswordData: { id: number, oldPassword: string, newPassword: string }) => {
+    return request.post('/user/ChangePassword', JSON.stringify(PasswordData))
+}
+
+/**
+ * 修改昵称
+ * @param nicknameData 新昵称数据
+ */
+export const changeNickname = (nicknameData: { id: number, newNickname: string }) => {
+    return request.put('/user/changeNickName', JSON.stringify(nicknameData))
+}
+
+/**
+ * 上传头像
+ */
+export const uploadAvatar = () => {
+
 }
 
 export const getContactList = () => {
