794d223df65b6483dd9435ebea02eb2d73703345
LvXinming <1533159440@qq.com>
2023-06-08 10:38:01 +0800

    添加上传组件与其测试

---
+++ src/views/test/upload.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Upload :isImg="true" :is-cropper="true"></Upload>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Upload from '@/components/Upload';
+</script>
+
+<style scoped>
+
+</style>
