52dc696d1f24be6717210cd15426807c8360ef9a
LvXinming <1533159440@qq.com>
2023-06-08 10:38:30 +0800

    消息和用户类型修改

---
+++ src/types/Contact.d.ts
@@ -0,0 +1,7 @@
+export interface ContactList {
+    id: number
+    avatar: string;
+    username: string;
+    nickname: string;
+    status?: number
+}
--- src/types/User.d.ts
+++ src/types/User.d.ts
@@ -1,5 +1,6 @@
 export interface User {
     id: number,
+    nickname: string,
     username: string,
     avatar: string
 }
