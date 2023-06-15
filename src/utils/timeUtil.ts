596725fb810345fc4c630c3a9adbcd3eedb2f3e3
LvXinming <1533159440@qq.com>
2023-06-15 19:05:51 +0800

    时间格式化修改

--- src/utils/timeUtil.ts
+++ src/utils/timeUtil.ts
@@ -1,10 +1,11 @@
 /**
  * 日期格式化
  * @param timestamp 时间戳
+ * @returns 返回格式化后日期
  */
-export const formatDate = (timestamp: number) => {
+export const formatDate = (timestamp: number): string => {
     if (!timestamp) {
-        return '.';
+        return '...';
     }
 
     let fixedDuration = 3 * 60 * 1000;
@@ -14,6 +15,23 @@ export const formatDate = (timestamp: number) => {
     } else if (duration < fixedDuration) {
         return `${(duration / (60 * 1000)).toFixed()}分钟前`
     }
-    let date = new Date(timestamp).toLocaleString('chinese', { hour12: false });
+    let date = new Date(timestamp).toLocaleString('chinese', { hour12: false,dateStyle:'short',timeStyle:'short' });
     return date;
+}
+
+/**
+ * 类似睡眠函数，使程序暂时停顿运行。配合async和await使用。
+ * @param ms 毫秒数
+ * @returns `then`返回毫秒数
+ */
+export const sleep = (ms: number) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    })
+}
+
+export default {
+    sleep, formatDate
 }
